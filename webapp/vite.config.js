16a3892360b586dfcfaee26bae5cbf066548034a
Daniel Baggio <danielbaggio@gmail.com>
2026-04-20 14:40:57 -0300

    vite para o netlify

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/booksummarytest/' : '/',
+  base: process.env.NETLIFY ? "/" : (process.env.NODE_ENV === "production" ? "/booksummarytest/" : "/"),
 })
